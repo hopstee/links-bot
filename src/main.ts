9aae4cf94626f2fc65bba4b6faa06318341dc4a9
Edik <hopsteee@gmail.com>
2025-08-07 18:30:21 +0300

    Добавил регистрацию вебхук хендлера

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config';
+import { Telegraf } from 'telegraf';
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
-    await app.listen(3000)
+    const app = await NestFactory.create(AppModule);
+
+    const bot = app.get(Telegraf);
+
+    const hookPath = '/bot-webhook';
+
+    app.use(hookPath, bot.webhookCallback(hookPath));
+
+    await app.listen(3000);
 }
 bootstrap()
