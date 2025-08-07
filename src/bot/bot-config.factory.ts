9da497ddb0c789b77a972fe35ead1f8a3cf5a7f4
Edik <hopsteee@gmail.com>
2025-08-07 17:34:07 +0300

    Добавил поддержку вебхука

--- src/bot/bot-config.factory.ts
+++ src/bot/bot-config.factory.ts
@@ -5,9 +5,51 @@ import { session } from 'telegraf';
 export const options = (): TelegrafModuleAsyncOptions => {
     return {
         inject: [ConfigService],
-        useFactory: (config: ConfigService) => ({
-            token: config.get('TELEGRAM_BOT_API_TOKEN'),
-            middlewares: [session()],
-        }),
+        useFactory: async (config: ConfigService) => {
+            const isProd = config.get('NODE_ENV') === 'production';
+            const token = config.get<string>('TELEGRAM_BOT_API_TOKEN');
+            const domain = config.get<string>('WEBHOOK_DOMAIN');
+            const hookPath = `/bot${token}`;
+            const fullWebhookUrl = `${domain}${hookPath}`;
+
+            const baseConfig = {
+                token,
+                middlewares: [session()],
+            }
+
+            if (isProd) {
+                try {
+                    const setRes = await fetch(`https://api.telegram.org/bot${token}/setWebhook`, {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({ url: fullWebhookUrl }),
+                    });
+
+                    const setData = await setRes.json();
+                    console.log('[Webhook setWebhook]', setData);
+
+                    const infoRes = await fetch(`https://api.telegram.org/bot${token}/getWebhookInfo`);
+                    const infoData = await infoRes.json();
+                    console.log('[Webhook getWebhookInfo]', infoData);
+                } catch (error) {
+                    console.error('[Webhook Error]', error);
+                }
+
+                return {
+                    ...baseConfig,
+                    launchOptions: {
+                        webhook: {
+                            domain,
+                            hookPath,
+                        },
+                    },
+                    telegram: {
+                        webhookReply: true,
+                    },
+                };
+            }
+
+            return baseConfig;
+        },
     };
 };
